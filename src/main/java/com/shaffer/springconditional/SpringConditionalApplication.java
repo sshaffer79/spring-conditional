e021c614bec24ae6f37fcf85b5e0a6a0613b3843
Seth Shaffer <seth@Seths-MacBook-Pro.local>
2019-02-08 09:14:38 -0500

    First commit

---
+++ src/main/java/com/shaffer/springconditional/SpringConditionalApplication.java
@@ -0,0 +1,14 @@
+package com.shaffer.springconditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConditionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConditionalApplication.class, args);
+	}
+
+}
+
